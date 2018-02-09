ea19b15dc9a946098474211893625d304bf9aac6
natelohn <nlohn@stanford.edu>
2018-02-09 09:47:46 -0800

    implementing schedule building

--- schedules/models.py
+++ schedules/models.py
@@ -7,23 +7,11 @@ from . import fields
 
 User = settings.AUTH_USER_MODEL
 
-# Helper methods
-def get_default_start_time():
-		now = dt.datetime.now()
-		default_start = now.replace(minute=0, second=0)
-		return default_start
-
-def get_default_end_time():
-		now = dt.datetime.now()
-		end_hour = now.hour + 1
-		default_end = now.replace(hour=end_hour, minute=0, second=0)
-		return default_end
-
 
 # My models
 class Qualification(models.Model):
 	user 			= models.ForeignKey(User, default=1, on_delete=models.CASCADE)
-	# ^Need to change to give each user their own DB
+	# ^ Need to change to give each user their own DB
 	active			= models.BooleanField(default=True)
 	title 			= models.CharField(max_length=100, unique=True, default='QUALIFICATION')
 
@@ -36,7 +24,7 @@ class Qualification(models.Model):
 
 class Flag(models.Model):
 	user 			= models.ForeignKey(User, default=1, on_delete=models.CASCADE)
-	# ^Need to change to give each user their own DB
+	# ^ Need to change to give each user their own DB
 	active			= models.BooleanField(default=True)
 	title 			= models.CharField(max_length=100, unique=True,default='TYPE OF SHIFT')
 
@@ -45,6 +33,7 @@ class Flag(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('settings')
+
 
 class Stapher(models.Model):
 	user 			= models.ForeignKey(User, default=1, on_delete=models.CASCADE)
@@ -58,8 +47,6 @@ class Stapher(models.Model):
 	age 			= models.IntegerField(default=18)
 	class_year 		= models.IntegerField(default=dt.datetime.today().year + 3)
 	summers_worked 	= models.IntegerField(default=0)
-	# Need to show shifts stapher is covering...
-	# OneToManyField ??
 
 	def __str__(self):
 		return f'{self.first_name} {self.last_name}'
@@ -75,25 +62,53 @@ class Shift(models.Model):
 	title 			= models.CharField(max_length=100,default='NAME OF SHIFT')
 	flags 			= models.ManyToManyField(Flag, blank=False)
 	day 			= fields.DayOfTheWeekField(default=0)
-	start			= models.TimeField(default=get_default_start_time)
-	end		 		= models.TimeField(default=get_default_end_time)
+	start			= models.TimeField(default=dt.datetime(2018, 1, 1, 11, 0, 0, 0))
+	end		 		= models.TimeField(default=dt.datetime(2018, 1, 1, 12, 0, 0, 0))
 	qualifications	= models.ManyToManyField(Qualification, blank=True)
 	workers_needed	= models.IntegerField(default=1)
 	difficult		= models.BooleanField(default=False)
-	# Need to show stapher(s) covering the shifts...
-	# OneToManyField ??
 
 	def __str__(self):
-		return f'{self.title}'
+		return f'{self.title} on {self.day}, {self.start} to {self.end}.'
+
+	def save(self, *args, **kwargs):
+		# Start of shift must be before the end of shift and day must be between 0 and 6 (Sun-Sat)
+		if self.start < self.end and self.day in range(0,6):
+			super(Shift, self).save(*args, **kwargs)
+		else:
+			return
 
 	def get_absolute_url(self):
 		return reverse('schedules:shift-detail', kwargs={'pk': self.id})
 
 
+# A class representing a single pair of Shift & Stapher
+class Staphing(models.Model):
+	user 			= models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+	# ^ Need to change to give each user their own DB
+	stapher 		= models.ForeignKey(Stapher, on_delete=models.CASCADE)
+	shift 			= models.ForeignKey(Shift, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f'{self.stapher.first_name}: {self.shift}'
+
+
+# A class representing all shift/staph pairs for a user - this is to allow for multiple schedules in the future
 class Schedule(models.Model):
 	user 			= models.ForeignKey(User, default=1, on_delete=models.CASCADE)
-	# ^Need to change to give each user their own DB
+	# ^ Need to change to give each user their own DB
 	active			= models.BooleanField(default=True)
+	title 			= models.CharField(max_length=100,default='NAME OF SCHEDULE')
+	staphings		= models.ManyToManyField(Staphing)
+
+	def __str__(self):
+		return f'{self.title}'
+
+
+
+
+
+
 
 
 
--- schedules/tests.py
+++ schedules/tests.py
@@ -3,7 +3,7 @@
 # from django.utils import timezone
 from django.test import TestCase
 
-from .models import Stapher
+from .models import Stapher, Shift
 
 
 class StapherModelTests(TestCase):
@@ -13,3 +13,26 @@ class StapherModelTests(TestCase):
         << ADD COMMENTS >>
         """
         self.assertIs(True, True)
+
+
+class ShiftModelTests(TestCase):
+
+    def test_end_time_is_before_start_time(self):
+        """
+        Test to make sure that Shifts can't have a start time after the end time.
+        """
+        s = Shift(start=1, end=0)
+        s.save()
+        qs = Shift.objects.all()
+        self.assertQuerysetEqual(qs, [])
+
+    def test_day_is_not_in_range(self):
+        """
+        Test to make sure that Shifts can't have a day that is not in the range of 0-6
+        """
+        s1 = Shift(day=7)
+        s2 = Shift(day=-1)
+        s1.save()
+        s2.save()
+        qs = Shift.objects.all()
+        self.assertQuerysetEqual(qs, [])
