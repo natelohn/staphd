f5b0ff5060cca49feb494416eb04d3b696c26a0d
natelohn <nlohn@stanford.edu>
2018-04-25 16:23:38 -0700

    testing live site pt. 2

--- staphd/settings/production_settings.py
+++ staphd/settings/production_settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'not_my_secret_key*$-%%not_my_secret_key*n&it0xg2lkf9!mz*&not_my_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
