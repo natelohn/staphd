b4bfb257be5caa7c7454d37ce62e4317c095824c
natelohn <nlohn@stanford.edu>
2018-04-25 11:05:50 -0700

    configured project for production enviroment on Heroku

--- staphd/settings/__init__.py
+++ staphd/settings/__init__.py
@@ -1,6 +1,4 @@
-from .production_settings import *
-
-# try:
-# 	from .local_settings import *
-# except:
-# 	from .production_settings import *
+try:
+	from .local_settings import *
+except:
+	from .production_settings import *
