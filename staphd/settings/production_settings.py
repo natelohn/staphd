32b445f011cd7882021d38463305098c3452d0a5
natelohn <nlohn@stanford.edu>
2018-04-26 14:02:49 -0700

    simple CACHE setting

--- staphd/settings/production_settings.py
+++ staphd/settings/production_settings.py
@@ -90,21 +90,11 @@ DATABASES = {
 }
 
 # Cache Configuration
-import os
-import urlparse
-import json
-
 CACHES = {
     'default': {
-        'BACKEND': 'django_bmemcached.memcached.BMemcached',
-        'LOCATION': os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
-        'OPTIONS': {
-                    'username': os.environ.get('MEMCACHEDCLOUD_USERNAME'),
-                    'password': os.environ.get('MEMCACHEDCLOUD_PASSWORD')
-            }
+        'BACKEND': 'django_bmemcached.memcached.BMemcached'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
