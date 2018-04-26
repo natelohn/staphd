bc5a60725307eb4d1cdf5b78f920fb133ae2da8b
natelohn <nlohn@stanford.edu>
2018-04-26 11:44:53 -0700

    updated celery config app imported

--- staphd/settings/production_settings.py
+++ staphd/settings/production_settings.py
@@ -189,7 +189,8 @@ LOGGING = {
 }
 
 # App Configurations for Celery on Redis
-staphd.conf.update(BROKER_URL=os.environ['REDIS_URL'], CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+from staphd.celery import app
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'], CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
