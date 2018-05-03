46565406c820b9a685f1e87f830f14d8907a670c
natelohn <nlohn@stanford.edu>
2018-05-02 18:35:34 -0700

    redis backend testing 8

--- schedules/tasks.py
+++ schedules/tasks.py
@@ -12,8 +12,8 @@ from .models import Settings as ScheduleSettings
 from .sort import get_sorted_shifts
 
 
-@shared_task(bind=True)
-def test_task(self):
+@shared_task(track_started=True)
+def test_task():
 	print('Task Happened!')
 	return 'Hulk Smash'
 
