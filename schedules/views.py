ae017f74146c6d5e5a1e1d149848da69b3021e65
natelohn <nlohn@stanford.edu>
2018-05-04 11:36:45 -0700

    updating files dir

--- schedules/views.py
+++ schedules/views.py
@@ -90,7 +90,7 @@ def download_file(request, filename):
 	# TODO: add MEDIA ROOT for production
 	# file_path = os.path.join(settings.MEDIA_ROOT, path)
 	
-	file_path = '../output/' + filename
+	file_path = '/static/' + filename
 	if os.path.exists(file_path):
 		with open(file_path, 'rb') as file:
 			# TODO: add MEDIA ROOT for production
