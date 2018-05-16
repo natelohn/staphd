281ce2325278f10356944c171716b0df010779f6
natelohn <nlohn@stanford.edu>
2018-05-16 13:57:29 -0700

    new file path for downloading files DEBUG

--- schedules/views.py
+++ schedules/views.py
@@ -91,6 +91,7 @@ def download_file(request, filename):
 	# file_path = os.path.join(settings.MEDIA_ROOT, path)
 	
 	file_path = '/app/static/xlsx/' + filename
+	print(f'file_path = {file_path}')
 	if os.path.exists(file_path):
 		with open(file_path, 'rb') as file:
 			# TODO: add MEDIA ROOT for production
@@ -99,11 +100,14 @@ def download_file(request, filename):
 			response = HttpResponse(file.read(), content_type="application/xlsx")
 			response['Content-Disposition'] = 'inline; filename=' + filename
 			return response
+	cd = os.getcwd()
+	print(f'dir = {cd}')
 	os.chdir('static/xlsx')
 	cd = os.getcwd()
 	print(f'dir = {cd}')
 	ls = os.listdir()
-	print(f'ls = {ls}')
+	for f in ls:
+		print(f'	{f}')
 	raise Http404
 
 @login_required
