0ffe1b4bd77f18cace6fd38dc7f6d89febf71f6c
natelohn <nlohn@stanford.edu>
2018-05-16 19:10:18 -0700

    adding all  s3 config to excel.py

--- schedules/views.py
+++ schedules/views.py
@@ -91,14 +91,13 @@ def download_file(request, filename):
 		path = 'app/static/xlsx/' + filename
 		print(f'filename = {filename}')
 		print(f'path = {path}')
-		s3.Bucket('staphd').download_file(filename, path)
-		print(f'path = {path}')
-		print(f'{path}')
 		cd = os.getcwd()
 		print(f'cd = {cd}')
 		ls = os.listdir()
 		for f in ls:
 			print(f' -{f}')
+		s3.Bucket('staphd').download_file(filename, path)
+
 		with open(path, 'rb') as file:
 			print('here')
 			response = HttpResponse(file.read(), content_type="application/xlsx")
