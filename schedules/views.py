f00a5d1a06beae8a74f4d2670a14978f981d6ccb
natelohn <nlohn@stanford.edu>
2018-05-18 17:58:46 -0700

    adding schedule list view

--- schedules/views.py
+++ schedules/views.py
@@ -618,8 +618,10 @@ class ScheduleCreate(LoginRequiredMixin, CreateView):
 class ScheduleList(LoginRequiredMixin, ListView):
 	template_name = 'schedules/schedule_list.html'
 
-	def get_queryset(self, *args, **kwargs):	
-		return Schedule.objects.all().order_by(Lower('title'))
+	def get_queryset(self, *args, **kwargs):
+		all_schedules = Schedule.objects.all().order_by(Lower('title'))
+		print(all_schedules)
+		return all_schedules
 
 	def get_context_data(self, *args, **kwargs):
 		context = super(ScheduleList, self).get_context_data(*args, **kwargs)
