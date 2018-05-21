7e53b257ed3e65435e038add112153b42a872fc6
natelohn <nlohn@stanford.edu>
2018-05-21 10:16:33 -0700

    adding schedule detail view

--- schedules/urls.py
+++ schedules/urls.py
@@ -19,10 +19,10 @@ urlpatterns = [
     url(r'^flag/(?P<pk>[\d]+)/delete$', views.FlagDelete.as_view(), name='flag-delete'),
 
     url(r'^schedules/$', views.build_view, name='schedule'),
-    url(r'^schedules/building$', views.build_schedules, name='building'),
-    url(r'^schedules/update$', views.update_files, name='update'),
-    url(r'^schedules/track$', views.track_state, name='track'),
-    url(r'^schedules/select$', views.ScheduleList.as_view(), name='select'),
+    url(r'^schedules/building/$', views.build_schedules, name='building'),
+    url(r'^schedules/update/$', views.update_files, name='update'),
+    url(r'^schedules/track/$', views.track_state, name='track'),
+    url(r'^schedules/select/$', views.ScheduleList.as_view(), name='select'),
     url(r'^schedules/select/(?P<pk>[\d]+)/$', views.schedule_selected, name='schedule-selected'),
     url(r'^schedules/create$', views.ScheduleCreate.as_view(), name='schedule-create'),
     url(r'^schedules/(?P<pk>[\d]+)/$', views.ScheduleDetail.as_view(), name='schedule-detail'),
@@ -36,17 +36,17 @@ urlpatterns = [
     url(r'^shifts/$', views.ShiftList.as_view(), name='shift-list'),
     url(r'^shifts/create/$', views.ShiftCreate.as_view(), name='shift-create'),
     url(r'^shifts/(?P<pk>[\d]+)/$', views.ShiftDetail.as_view(), name='shift-detail'),
-    url(r'^shifts/(?P<pk>[\d]+)/edit$', views.ShiftUpdate.as_view(), name='shift-update'),
-    url(r'^shifts/(?P<pk>[\d]+)/delete$', views.ShiftDelete.as_view(), name='shift-delete'),
-    url(r'^shifts/(?P<sort>[\D]*)/(?P<key>[\d]*)$', views.ShiftList.as_view(), name='shift-list-sort'),
+    url(r'^shifts/(?P<pk>[\d]+)/edit/$', views.ShiftUpdate.as_view(), name='shift-update'),
+    url(r'^shifts/(?P<pk>[\d]+)/delete/$', views.ShiftDelete.as_view(), name='shift-delete'),
+    url(r'^shifts/(?P<sort>[\D]*)/(?P<key>[\d]*)/$', views.ShiftList.as_view(), name='shift-list-sort'),
 
     url(r'^staphers/$', views.StapherList.as_view(), name='stapher-list'),
     url(r'^staphers/create/$', views.StapherCreate.as_view(), name='stapher-create'),
     url(r'^staphers/(?P<pk>[\d]+)/$', views.StapherDetail.as_view(), name='stapher-detail'),
-    url(r'^staphers/(?P<pk>[\d]+)/edit$', views.StapherUpdate.as_view(), name='stapher-update'),
-    url(r'^staphers/(?P<pk>[\d]+)/delete$', views.StapherDelete.as_view(), name='stapher-delete'),
+    url(r'^staphers/(?P<pk>[\d]+)/edit/$', views.StapherUpdate.as_view(), name='stapher-update'),
+    url(r'^staphers/(?P<pk>[\d]+)/delete/$', views.StapherDelete.as_view(), name='stapher-delete'),
 
     url(r'^qualification/create/$', views.QualificationCreate.as_view(), name='qualification-create'),
-    url(r'^qualification/(?P<pk>[\d]+)/delete$', views.QualificationDelete.as_view(), name='qualification-delete')
+    url(r'^qualification/(?P<pk>[\d]+)/delete/$', views.QualificationDelete.as_view(), name='qualification-delete')
 
 ]
