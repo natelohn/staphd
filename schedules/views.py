12702200a26379fb103bb9d95395227a2b45878d
natelohn <nlohn@stanford.edu>
2018-05-22 12:55:29 -0700

    editing build process to use the same schedule over again

--- schedules/views.py
+++ schedules/views.py
@@ -31,7 +31,11 @@ class HomeView(LoginRequiredMixin, TemplateView):
 	template_name = 'home.html'	
 
 	def get_context_data(self, *args, **kwargs):
-		cache.set('schedule_id', None, 0) #TODO: Delete < 
+		test = cache.get('schedule_id') #TODO: Delete < 
+		print(f'test = {test}') #TODO: Delete < 
+		cache.set('current_task_id', None, 0) #TODO: Delete < 
+		test = cache.get('current_task_id') #TODO: Delete < 
+		print(f'test = {test}')#TODO: Delete < 
 		context = super(HomeView, self).get_context_data(*args, **kwargs)
 		try:
 			schedule = Schedule.objects.get(active__exact = True)
