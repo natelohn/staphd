1a9aaa86d76544c3ddbf71bee16721f745e257d6
natelohn <nlohn@stanford.edu>
2018-05-23 15:36:00 -0700

    adding schedule settings view - select form

--- schedules/models.py
+++ schedules/models.py
@@ -327,7 +327,7 @@ class Parameter(models.Model):
 	function_id		= models.IntegerField(unique = True, default = 1)
 
 	def __str__(self):
-		return f'{self.title}'
+		return f'Title: {self.title} 	Description: {self.description}'
 
 class Settings(models.Model):
 	parameters		= models.ManyToManyField(Parameter, blank = False)
