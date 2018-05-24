8f31da88671c0960a34c44165925687e0a3ac9d3
natelohn <nlohn@stanford.edu>
2018-05-24 07:43:26 -0700

    adding schedule settings view - auto form autoschedul

--- schedules/fields.py
+++ schedules/fields.py
@@ -22,8 +22,8 @@ GENDER = {
 }
 
 TIE_OPTIONS = {
-    0 : 'Choose one of the people who tied at random.',
-    1 : 'Pick whoever won highest ranked parmeter.',
+    0 : 'Automatically choose one of the people who tied at random.',
+    1 : 'Automatically pick whoever won highest ranked parmeter.',
     2 : 'I want to choose whenever there is a tie.'
 }
 
