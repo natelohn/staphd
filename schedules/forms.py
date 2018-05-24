61c6f431001e1737def4cbe1f05ee3ceb18592d5
natelohn <nlohn@stanford.edu>
2018-05-24 16:13:34 -0700

    adding schedule settings view - select form table

--- schedules/forms.py
+++ schedules/forms.py
@@ -169,6 +169,7 @@ class SettingsPreferenceForm(forms.ModelForm):
 			min_auto_threshold = 1
 			max_auto_threshold = ScheduleBuildingSettings.objects.get().parameters.all().count()
 			if auto_threshold < min_auto_threshold:
+				print('Huh?')
 				raise forms.ValidationError(f"A thresholds minimum possible value is {min_auto_threshold}.")
 			if auto_threshold > max_auto_threshold:
 				raise forms.ValidationError(f'You have {max_auto_threshold} active parameters. A threshold greater than {max_auto_threshold} is not allowed.')
