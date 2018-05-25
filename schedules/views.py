05fe2eb354cc5c590f47589e8d4e58be8bb8eaa9
natelohn <nlohn@stanford.edu>
2018-05-25 11:16:57 -0700

    adding stapher schedule view

--- schedules/views.py
+++ schedules/views.py
@@ -439,14 +439,15 @@ def schedule_view(request, *args, **kwargs):
 		row_for_time = [time]
 		for i, day in enumerate(days):
 			shift = stapher.get_shift_during_time(i, t, staphings)
-			if shift and shift.id not in seen_shifts:
+			if not shift:
+				row_for_time.append(False)
+			elif shift.id not in seen_shifts:
 				cell = {}
 				cell['title'] = f'{shift.title}, {get_readable_time(shift.start)}-{get_readable_time(shift.end)}'
 				cell['span'] = get_hours_from_timedelta(shift.length()) * 4
 				row_for_time.append(cell)
 				seen_shifts.add(shift.id)
-			else:
-				row_for_time.append(False)
+				
 		all_rows_for_time.append(row_for_time)
 		time += increment
 
