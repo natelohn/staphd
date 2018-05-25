9ec87950b83913785c62d29a0b7b8793bc15ec8e
natelohn <nlohn@stanford.edu>
2018-05-25 15:00:34 -0700

    adding stapher schedule view

--- schedules/view_helpers.py
+++ schedules/view_helpers.py
@@ -9,22 +9,23 @@ def get_week_schedule_view_info(stapher, staphings):
 	max_time = datetime.timedelta(hours = 23, minutes = 30)
 	increment = datetime.timedelta(hours = 0, minutes = 15)
 	all_rows_for_time = [days]
-	seen_shifts = set()
+	seen_staphings = set()
 	while time <= max_time:
 		hours = int(get_hours_from_timedelta(time))
 		minutes = (time.seconds//60)%60
 		t = datetime.time(hours, minutes, 0, 0)
 		row_for_time = []
 		for i, day in enumerate(days):
-			shift = stapher.get_shift_during_time(i, t, staphings)
-			if not shift:
+			staphing = stapher.get_staphing_during_time(i, t, staphings)
+			if not staphing:
 				row_for_time.append(False)
-			elif shift.id not in seen_shifts:
+			elif staphing.id not in seen_staphings:
 				cell = {}
-				cell['title'] = f'{shift.title}, {get_readable_time(shift.start)}-{get_readable_time(shift.end)}'
-				cell['span'] = get_hours_from_timedelta(shift.length()) * 4
+				cell['title'] = f'{staphing.shift.title}, {get_readable_time(staphing.shift.start)}-{get_readable_time(staphing.shift.end)}'
+				cell['span'] = get_hours_from_timedelta(staphing.shift.length()) * 4
+				cell['staphing_id'] = staphing.id
 				row_for_time.append(cell)
-				seen_shifts.add(shift.id)
+				seen_staphings.add(staphing.id)
 				
 		all_rows_for_time.append(row_for_time)
 		time += increment
