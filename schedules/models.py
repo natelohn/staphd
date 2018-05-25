0cd9912ffb935ad0defa95e48f2a0902419a7213
natelohn <nlohn@stanford.edu>
2018-05-25 15:39:51 -0700

    delete staphing  view

--- schedules/models.py
+++ schedules/models.py
@@ -327,7 +327,7 @@ class Staphing(models.Model):
 	schedule 		= models.ForeignKey(Schedule, on_delete = models.CASCADE)
 
 	def __str__(self):
-		return f'{self.stapher.first_name} working {self.shift}'
+		return f"{self.shift} from {self.stapher.first_name}'s schedule on \"{self.schedule}\""
 
 	def get_absolute_url(self):
 		return reverse('schedules:stapher-schedule', kwargs={'pk':self.stapher.id})
