afb00d5cd322d54443a4552566ca0fa90cf29047
natelohn <nlohn@stanford.edu>
2018-05-30 11:39:57 -0700

    adding shifts form

--- schedules/views.py
+++ schedules/views.py
@@ -556,7 +556,7 @@ def stapher_schedule_add(request, *args, **kwargs):
 		if form.is_valid():
 			# process the data in form.cleaned_data as required
 			print('Hereee...')
-			print(request)
+			print(form)
 			# redirect to a new URL:
 			return HttpResponseRedirect(reverse('schedules:stapher-schedule', kwargs={'pk': stapher.id}))
 	
