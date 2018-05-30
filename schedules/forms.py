ae6df13ee906f3cba0014165a39cc10225c371b4
natelohn <nlohn@stanford.edu>
2018-05-30 15:44:02 -0700

    adding shifts form

--- schedules/forms.py
+++ schedules/forms.py
@@ -211,7 +211,7 @@ class SettingsPreferenceForm(forms.ModelForm):
 
 class AddShiftsForm(forms.Form):
 	your_name = forms.CharField(label='Your name', max_length=100)
-	shifts = forms.CheckboxSelectMultiple()
+	shifts = forms.MultipleChoiceField(choices = ['a', 'ab', 'b'])
 
 	class Meta:
 		fields = ['shifts']
