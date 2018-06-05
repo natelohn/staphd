59efba12d375ce864f95eb8f43de3fb1d6421126
natelohn <nlohn@stanford.edu>
2018-06-04 18:12:26 -0700

    debug schedule building

--- schedules/views.py
+++ schedules/views.py
@@ -33,6 +33,7 @@ class HomeView(LoginRequiredMixin, TemplateView):
 	template_name = 'home.html'
 
 	def get_context_data(self, *args, **kwargs):
+
 		context = super(HomeView, self).get_context_data(*args, **kwargs)
 		try:
 			schedule = Schedule.objects.get(active__exact = True)
