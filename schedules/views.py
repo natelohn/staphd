3e2a8d6986b8be3f8e908ccc01778c0401448c96
natelohn <nlohn@stanford.edu>
2018-06-04 18:00:37 -0700

    debug schedule building

--- schedules/views.py
+++ schedules/views.py
@@ -186,19 +186,18 @@ class SettingPreferenceUpdate(LoginRequiredMixin, UpdateView):
 @csrf_exempt
 def build_schedules(request, *args, **kwargs):
 	task_id = cache.get('current_task_id')
-	context = {}
 	if not task_id:
 		try:
 			schedule = Schedule.objects.get(active__exact = True)
+			schedule_id = schedule.id
+			task = build_schedules_task.delay(schedule_id)
+			task_id = task.task_id
+			cache.set('current_task_id', task_id, 3000)
 		except:
 			return render(request,'schedules/schedule.html', {'schedule_error_message':'Must select a schedule first.'})
-		context['schedule'] = schedule.title
-		schedule_id = schedule.id
-		task = build_schedules_task.delay(schedule_id)
-		task_id = task.task_id
-		cache.set('current_task_id', task_id, 3000)
 	request.session['task_id'] = task_id
-	context['task_id'] = task_id
+	context = {'task_id':task_id}
+	context['schedule'] = schedule.title
 	return render(request,'schedules/progress.html', context)
 
 @login_required
