127b93a657809a73269fe11769a0a8edacb7ee51
natelohn <nlohn@stanford.edu>
2018-06-06 10:44:05 -0700

    adding ShiftSet model

--- schedules/models.py
+++ schedules/models.py
@@ -369,7 +369,6 @@ class Settings(models.Model):
 
 	def user_breaks_ties(self):
 		return int(self.tie_breaker) == 2
-		
 
 class Master(models.Model):
 	title 			= models.CharField(max_length = 100, default = 'NAME OF MASTER')
