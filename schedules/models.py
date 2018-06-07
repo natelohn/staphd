d5535de4c23881089ba87f6c217d9b5f210b542e
natelohn <nlohn@stanford.edu>
2018-06-07 13:35:55 -0700

    adding ShiftSet model form

--- schedules/models.py
+++ schedules/models.py
@@ -330,7 +330,7 @@ class Schedule(models.Model):
 		total_needed = 0
 		for shift in shifts_in_set:
 			total_needed += shift.workers_needed
-		percent_complete = int((len(staphings) / total_needed)  * 100)
+		percent_complete = int((len(staphings) / total_needed)  * 100) if total_needed else 0
 		return percent_complete
 
 	
