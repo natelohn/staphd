1caef13a2698816e08743098b11dd2c9b7f0387e
natelohn <nlohn@stanford.edu>
2018-06-07 16:42:19 -0700

    ratio view

--- schedules/ratio.py
+++ schedules/ratio.py
@@ -1,5 +1,5 @@
 
 def get_solution_for_time(shifts, staphers, day, time):
-	shifts.filter(day__exact = time, start__lte = end, end__gt = time).order_by('workers_needed')
+	shifts.filter(day__exact = day, start__lte = time, end__gt = time).order_by('workers_needed')
 	for shift in shifts:
 		print(shift)
