78d0315d8bf9886384bdc01d0a71ee0203019953
natelohn <nlohn@stanford.edu>
2018-06-07 18:14:46 -0700

    ratio view

--- schedules/ratio.py
+++ schedules/ratio.py
@@ -5,14 +5,21 @@ from .sort import get_ordered_start_and_end_times_by_day, get_qual_and_shifts_di
 def get_solution(staphings, day, start, end,shift_dict, stapher_dict):
 	for key in shift_dict:
 		shifts_in_window = []
+		print(f'pre-loop shifts: {shifts_in_window}')
 		for s in shift_dict[key]:
+			print(s)
 			if s.is_in_window(day, start, end):
 				shifts_in_window.append(s)
+				print(f'mid-loop shifts: {shifts_in_window}')
+		print(f'post-loop shifts: {shifts_in_window}')
 		if shifts_in_window:
 			eligible_staphers = []
+			print(f'pre-loop staphers: {eligible_staphers}')
 			for s in stapher_dict[key]:
+				print(f'mid-loop staphers: {eligible_staphers}')
 				if s.is_free_during_window(staphings, day, start, end):
-					eligible_staphers.append(s)	
+					eligible_staphers.append(s)
+					print(f'edited staphers: {eligible_staphers}')
 			print(eligible_staphers)
 			for shift in shifts_in_window:
 				print(f'{shift} - {shift.workers_needed} / {len(eligible_staphers)}')
