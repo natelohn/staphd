84e3d6fb19ed3e560621c7d3c72dc87e6f4660be
natelohn <nlohn@stanford.edu>
2018-06-07 17:52:09 -0700

    ratio view

--- schedules/ratio.py
+++ schedules/ratio.py
@@ -2,14 +2,10 @@ import datetime
 
 from .sort import get_ordered_start_and_end_times_by_day, get_qual_and_shifts_dicts, get_stapher_dict
 
-def get_solution(day, start, end, shift_dict, stapher_dict):
-	print(f'day = {day}')
+def get_solution(staphings, day, start, end,shift_dict, stapher_dict):
 	for key in shift_dict:
-		print(f'key = {key}')
 		shifts_in_window = [s for s in shift_dict[key] if s.is_in_window(day, start, end)]
-		print(f'key = {key}')
-		eligible_staphers = [s for s in stapher_dict[key] if s.is_free_during_window(day, start, end)]
-		print(f'eligible_staphers = {eligible_staphers}')
+		eligible_staphers = [s for s in stapher_dict[key] if s.is_free_during_window(staphings, day, start, end)]
 		for shift in shifts_in_window:
 			print(f'{shift} - {shift.workers_needed} / {len(eligible_staphers)}')
 
@@ -24,4 +20,4 @@ def find_ratios(shifts, staphers, staphings):
 		for i in range(1, len(ordered_times_by_day[day])):
 			start = ordered_times_by_day[day][i - 1]
 			end = ordered_times_by_day[day][i]
-			get_solution(day, start, end, shift_dict, stapher_dict)
+			get_solution(staphings, day, start, end, shift_dict, stapher_dict)
