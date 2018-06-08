6a1a539a4d75e20247ad1a4fbfb12e09eba9fe39
natelohn <nlohn@stanford.edu>
2018-06-07 17:10:26 -0700

    ratio view

--- schedules/ratio.py
+++ schedules/ratio.py
@@ -1,21 +1,16 @@
 import datetime
 
+from .sort import get_ordered_start_and_end_times_by_day
 
-def get_solution(shifts, staphers):
+def get_solution(shifts, staphers, free_and_qualified):
 	for shift in shifts:
-		print(f'{shift} - {shift.workers_needed}')
+		print(f'{shift} - {shift.workers_needed} / {free_and_qualified[shift.id]}')
 
 
 
 def find_ratios(shifts, staphers, staphings):
-	free_and_qualified = {}
-	for shift in shifts_in_window:
-		free_and_qualified[shift.id] = []
-		for stapher in staphers:
-			if stapher.can_cover(shift, staphings)
-	shifts.order_by('workers_needed')
-	day = 0
-	start = datetime.time(11, 0, 0, 0)
-	end = datetime.time(11, 15, 0, 0)
-	shifts_in_window = [s for s in shifts if s.is_in_window(day, start, end)]
-	get_solution(shifts_in_window, staphers)
+	ordered_times_by_day = get_ordered_start_and_end_times_by_day(shifts)
+	for day in ordered_times_by_day:
+		print(f'{day}')
+		for time in ordered_times_by_day[day]:
+			print(f'	- {time}')
