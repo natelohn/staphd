56e90187be057bc1dac4a493b5b7e3a150eac1ae
natelohn <nlohn@stanford.edu>
2018-06-07 20:56:49 -0700

    ratio view

--- schedules/ratio.py
+++ schedules/ratio.py
@@ -10,12 +10,19 @@ def clean_ratios(shifts, staphers, staphings):
 	shift_and_qual_dicts = get_qual_and_shifts_dicts(shifts)
 	shift_dict = shift_and_qual_dicts[1]
 	stapher_dict = get_stapher_dict(staphers, shift_and_qual_dicts[0])
-	q_sets = list(shift_dict.keys())
-	print(q_sets)
-	for i in range(1, len(q_sets)):
-		for item in list(combinations(q_sets, i)):
-			print(item)
-	print('--------------')
+	all_q_sets = list(shift_dict.keys())
+	print(all_q_sets)
+	for i in range(1, len(all_q_sets)):
+		for qs_set in list(combinations(all_q_sets, i)):
+			sum_of_workers_needed = 0
+			availible_workers = set()
+			for qs in qs_set:
+				sum_of_workers_needed += sum([s.workers_needed for s in shift_dict[q_set]])
+				availible_workers = availible_workers | set([s.id for s in stapher_dict[q_set]])
+				ratio = sum_of_workers_needed / availible_workers if availible_workers else sum_of_workers_needed + 1
+				if ratio > 1:
+					return False
+	return True
 
 
 def find_ratios(shifts, staphers, staphings):
@@ -25,5 +32,8 @@ def find_ratios(shifts, staphers, staphings):
 			start = ordered_times_by_day[day][i - 1]
 			end = ordered_times_by_day[day][i]
 			shifts_in_window = shifts.filter(day = day, start__lt = end, end__gt = start).order_by('workers_needed')
-			clean_ratios(shifts_in_window, staphers, staphings)
-
+			clean = clean_ratios(shifts_in_window, staphers, staphings)
+			if clean:
+				print('clean')
+			else:
+				print('*************** UNCLEAN!! *****************')
