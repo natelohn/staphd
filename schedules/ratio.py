e77acede268049e77c54862a486e4bd1bb3d229f
natelohn <nlohn@stanford.edu>
2018-06-07 21:10:32 -0700

    ratio view

--- schedules/ratio.py
+++ schedules/ratio.py
@@ -17,13 +17,10 @@ def clean_ratios(shifts, staphers, staphings):
 			sum_of_workers_needed = 0
 			availible_workers = set()
 			for qs in qs_set:
-				print('A')
 				sum_of_workers_needed += sum([s.workers_needed for s in shift_dict[qs]])
-				print('B')
 				availible_workers = availible_workers | set([s.id for s in stapher_dict[qs]])
-				print('C')
-				ratio = sum_of_workers_needed / availible_workers if availible_workers else sum_of_workers_needed + 1
-				print('D')
+				print(f'{sum_of_workers_needed} / {availible_workers}')
+				ratio = (sum_of_workers_needed / availible_workers) if availible_workers > 0 else (sum_of_workers_needed + 1)
 				if ratio > 1:
 					return False
 	return True
