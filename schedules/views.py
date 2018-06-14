88ea2e8605cfdf7d0fe42e7e7c7c56c7692152f6
natelohn <nlohn@stanford.edu>
2018-06-13 22:13:56 -0700

    shift covering assistance

--- schedules/views.py
+++ schedules/views.py
@@ -532,7 +532,7 @@ class StapherList(LoginRequiredMixin,ListView):
 								end = get_time_from_string(end_str)
 					free_during_time = [s for s in all_staphers if s.free_during_window(all_staphings, day, start, end)] if (day != None and start != None and end != None) else []
 					explanation_str = f'- are free on {query} on the \"{schedule}\" schedule' if schedule else f'- all staphers are free on {query} since no schedule is selected.'
-					if free_during_time: query_explanation.append(explanation_str)
+					if free_during_time: query_explanation += explanation_str
 
 					queryset = list(names_contain) + list(title_exact) + list(class_year_exact) + list(age_exact) + list(summers_exact) + free_during_time
 				filtered_query_set = list(set(filtered_query_set) & set(queryset))
