74832ca87891aa7237e9bce5c146e5046cb2fe6f
natelohn <nlohn@stanford.edu>
2018-08-21 19:57:43 -0700

    debugging build relocate

--- schedules/views.py
+++ schedules/views.py
@@ -104,7 +104,8 @@ def download_analytics(request, *args, **kwargs):
 def build_view(request, *args, **kwargs):
 	template = 'schedules/schedule.html'
 	cache.delete('current_task_id')
-	print f'CURRENT TASK = {cache.get('current_task_id')}'
+	current_task_id = cache.get('current_task_id')
+	print f'CURRENT TASK = {current_task_id}'
 	try:
 		schedule = Schedule.objects.get(active__exact = True)
 		context = {'schedule':schedule.title}
